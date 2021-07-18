872c0072e95d3660a15f42bd7dfa8e0c8da0a06d
Fabrizio Fubelli <fabrizio@fubelli.org>
2021-07-18 19:57:41 +0200

    Updated ibm whatson script

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json, os, sys, pandas
-from ibm_watson import NaturalLanguageUnderstandingV1
+import json
+import os
+import pandas as pd
+import sys
+
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import Features, ConceptsOptions
 
 
@@ -8,15 +12,15 @@ from ibm_watson.natural_language_understanding_v1 import Features, ConceptsOptio
 
 project_dir = os.path.dirname(sys.argv[0])
 input_dir = os.path.join(os.path.dirname(project_dir), 'input')
+out_dir = os.path.join(project_dir, 'out')
 commonlitreadabilityprize_input_dir = os.path.join(input_dir, 'commonlitreadabilityprize')
 
 with open(os.path.join(project_dir, 'config', 'ibm_credentials.json')) as ibm_credentials_fp:
     ibm_credentials = json.load(ibm_credentials_fp)
 
 with open(os.path.join(commonlitreadabilityprize_input_dir, 'train.csv')) as train_csv_fp:
-    train_csv = pandas.read_csv(train_csv_fp)
+    train_csv = pd.read_csv(train_csv_fp)
 
-print(train_csv)
 
 # ----------------------------------------------- IBM API integration ------------------------------------------------ #
 
@@ -29,14 +33,24 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
 natural_language_understanding.set_service_url(ibm_credentials.get('url'))
 
 
+# ------------------------------------------------- Script Execution ------------------------------------------------- #
+
+ibm_responses = [None] * len(train_csv)
+
+for train_csv_index, train_csv_row in train_csv.iterrows():
+    #  Call IBM API
+    ibm_response = natural_language_understanding.analyze(
+        text=train_csv_row.get('excerpt'),
+        features=Features(concepts=ConceptsOptions())
+    ).get_result()
+    # Save IBM response
+    ibm_responses[train_csv_index] = json.dumps(ibm_response, indent=2)
+    break
+
+# Set ibm_response column
+train_csv['ibm_response'] = ibm_responses
+
+# Write DataFrame CSV in ".out/" folder
+train_csv.to_csv(os.path.join(out_dir, 'train.csv'))
+
 exit(0)
-
-
-#  Call IBM API
-response = natural_language_understanding.analyze(
-    url='http://newsroom.ibm.com/Guerbet-and-IBM-Watson-Health-Announce-Strategic-Partnership-for-Artificial'
-        '-Intelligence-in-Medical-Imaging-Liver',
-    features=Features(concepts=ConceptsOptions())
-).get_result()
-
-print(json.dumps(response, indent=2))
---
+++ test.py
@@ -0,0 +1,21 @@
+import json
+import os
+import pandas as pd
+import sys
+
+
+# ----------------------------------------- Configuration and Output files ------------------------------------------- #
+
+project_dir = os.path.dirname(sys.argv[0])
+out_dir = os.path.join(project_dir, 'out')
+
+with open(os.path.join(out_dir, 'train.csv')) as train_csv_fp:
+    train_csv = pd.read_csv(train_csv_fp)
+
+
+# ------------------------------------------------- Script Execution ------------------------------------------------- #
+
+for train_csv_index, train_csv_row in train_csv.iterrows():
+    #  Read IBM response column
+    ibm_response = json.loads(train_csv_row.get('ibm_response'))
+    print(ibm_response)
