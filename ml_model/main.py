066bdaa2d1525253e1706c2737dabde5bf03574a
Fabrizio Fubelli <fabrizio@fubelli.org>
2021-07-22 19:14:55 +0200

    init ml

--- ml_model/main.py
+++ ml_model/main.py
@@ -4,6 +4,11 @@ import os
 
 import pandas as pd
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import mean_squared_error, r2_score
+
 project_dir = os.path.dirname(os.path.realpath(__file__))
 input_dir = os.path.join(os.path.dirname(project_dir), 'input')
 commonlitreadabilityprize_input_dir = os.path.join(input_dir, 'commonlitreadabilityprize')
@@ -11,7 +16,7 @@ commonlitreadabilityprize_input_dir = os.path.join(input_dir, 'commonlitreadabil
 with open(os.path.join(commonlitreadabilityprize_input_dir, 'train.csv')) as train_csv_fp:
     train_csv = pd.read_csv(train_csv_fp)
 
-with open(os.path.join(commonlitreadabilityprize_input_dir, 'train.csv')) as train_csv_fp:
-    test_csv = pd.read_csv(train_csv_fp)
+with open(os.path.join(commonlitreadabilityprize_input_dir, 'train.csv')) as test_csv_fp:
+    test_csv = pd.read_csv(test_csv_fp)
 
-
+exit(0)
