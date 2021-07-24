ce178b0ec6c7858d3ccf9dacc6f456e189bb9bbe
Fabrizio Fubelli <fabrizio@fubelli.org>
2021-07-24 15:38:58 +0200

    Updated py script

--- ml_model/main.py
+++ ml_model/main.py
@@ -6,11 +6,12 @@ import pandas as pd
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-
+from sklearn.naive_bayes import BernoulliNB
 
 project_dir = os.path.dirname(os.path.realpath(__file__))
 input_dir = os.path.join(os.path.dirname(project_dir), 'input')
@@ -22,15 +23,52 @@ with open(os.path.join(commonlitreadabilityprize_input_dir, 'train.csv')) as tra
 with open(os.path.join(commonlitreadabilityprize_input_dir, 'train.csv')) as test_csv_fp:
     test_csv_df = pd.read_csv(test_csv_fp)
 
-# X = train_csv_df[['excerpt', 'standard_error']]
-X = train_csv_df['excerpt']
-y = train_csv_df['target'].values
+# Show target statistics
+sns.histplot(train_csv_df, x='target')
+plt.show()
 
-vectorizer = CountVectorizer(ngram_range=(1, 2))
-X = vectorizer.fit_transform(X)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 
 exit(0)
 
+
+
+
+
+y = train_csv_df['target']
+X = train_csv_df['excerpt']
+
+countVectorizer = CountVectorizer(analyzer='word', ngram_range=(1, 2))
+countVectorizer.fit(X)
+X = countVectorizer.transform(X)
+
+print(countVectorizer.get_feature_names())
+print(X.todense())
+print(X)
+exit()
+# X2 = countVectorizer.fit_transform(X)
+# print(countVectorizer.get_feature_names())
+
+X_train, X_test, y_train, y_test = train_test_split(X2, y, random_state=0)
+model = BernoulliNB()
+model.fit(X_train, y_train)
+
+exit(0)
+# X = train_csv_df[['excerpt', 'standard_error']]
+
+print(train_csv_df.head())
+
+y = train_csv_df['target']
+
+model = BernoulliNB()
+model.fit(X_train, y_train)
+
+p_train = model.predict(X_train)
+p_test = model.predict(X_test)
+
+acc_train = mean_squared_error(y_train, p_train)
+acc_test = mean_squared_error(y_test, p_test)
+
+print(f'Train acc. {acc_train}; Test acc. {acc_test}')
+
+exit(0)
